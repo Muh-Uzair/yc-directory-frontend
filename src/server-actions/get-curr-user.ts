10a6dded0cbfeeb7af9cdc546a5abcb2f4009098
Muh-Uzair <muhammaduzair1062001@gmail.com>
2025-08-25 15:42:34 +0500

    try5

--- src/server-actions/get-curr-user.ts
+++ src/server-actions/get-curr-user.ts
@@ -3,37 +3,33 @@
 import { cookies } from "next/headers";
 
 export const getCurrUser = async () => {
-  try {
-    const cookieStore = await cookies();
+  const cookieStore = await cookies();
 
-    const jwt = cookieStore.get("jwt")?.value;
+  const jwt = cookieStore.get("jwt")?.value;
 
-    if (!jwt) {
-      return null;
-    }
-
-    const res = await fetch(`${process.env.BACKEND_URL}/users/curr`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${jwt}`,
-      },
-
-      credentials: "include",
-      cache: "force-cache",
-      next: {
-        tags: ["currUser"],
-      },
-    });
-
-    if (!res.ok) {
-      throw new Error();
-    }
-
-    const data = await res.json();
-
-    return data;
-  } catch (err: unknown) {
-    console.log(`Unable to fetch curr user Error => ${err}`);
+  if (!jwt) {
+    return null;
   }
+
+  const res = await fetch(`${process.env.BACKEND_URL}/users/curr`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwt}`,
+    },
+
+    credentials: "include",
+    cache: "force-cache",
+    next: {
+      tags: ["currUser"],
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error();
+  }
+
+  const data = await res.json();
+
+  return data;
 };
